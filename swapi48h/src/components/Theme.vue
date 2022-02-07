2ee45ed161a066df7a6e68b0f442064edc305996
Mathis <mathis.augereau@ynov.com>
2022-02-07 14:19:50 +0100

    add

---
+++ swapi48h/src/components/Theme.vue
@@ -0,0 +1,26 @@
+<script setup> 
+new Vue({
+    el: '#app',
+    data () {
+        return{
+            info:null
+        }
+    },
+    mounted (){
+        axios
+        .get('https://swapi.dev/api/')
+        .then(reponse =>(this.info = reponse))
+    }
+})
+</script>
+
+<template>
+<div id="app">
+    {{ info }}
+</div>
+
+</template>
+
+
+<style scoped>
+</style>
