7d8ad39a1ba1c2c8906739984032aae7da9ee991
AySqirl <aymericjacob@outlook.com>
2022-02-07 13:57:57 +0100

    upd store

--- swapi48h/src/store.js
+++ swapi48h/src/store.js
@@ -2,9 +2,22 @@ import { createStore } from "vuex";
 
 const store = createStore({
     state: {
+      listQuiz: [
+        /*
+        {
+          Q: 'laQuestion',
+          R: ['reponse1','reponse2','reponse3','reponse4'],
+          C: number, //en fonciton de R
+          A: number, //reponse de la personne
+        }
+        */
+      ],
 
     },
     mutations: {
+      addQuestionQuiz: (state, newQuestion) => {state.listQuiz.push(newQuestion)},
+    
+
     }
 })
 
