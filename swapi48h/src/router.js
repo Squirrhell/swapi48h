4c8162499db82105e80f0a55085ea1f34a475f19
AySqirl <aymericjacob@outlook.com>
2022-02-07 13:40:45 +0100

    add store + router

---
+++ swapi48h/src/router.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home.vue";
+import Theme from "./components/Theme.vue";
+import Wiki from "./components/Wiki.vue";
+import Quiz from "./components/Quiz.vue";
+
+const notFound = { template: '<div>Error 404</div>' };
+
+const routes = [
+    { 
+        path: '/Home' , 
+        name: 'Home', 
+        component: Home
+    },
+    { 
+        path: '/Theme', 
+        name: 'Theme', 
+        component: Theme
+    },
+    {
+        path: '/Wiki',
+        name: 'Wiki',
+        component: Wiki
+    },
+    {
+        path: '/Quiz',
+        name: 'Quiz',
+        component: Quiz
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
+
---
+++ swapi48h/src/store.js
@@ -0,0 +1,13 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+
+    },
+    mutations: {
+    }
+})
+
+export default store;
+
+// npm install vuex@next -- save
