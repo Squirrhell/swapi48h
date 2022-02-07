f17a25facb27afe07389897ca98a6498b7ae8bf4
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-02-07 14:08:22 +0100

    Ajout de la page Quiz : commencement
    Ajout de la page Quiz avec seulement des éléments de test, pour pouvoir voir la structure de base

---
+++ swapi48h/src/components/Quiz.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+
+})
+const actualQuestion = ref('');
+const numberOfQuestions = ref(10);
+const numberOfActualQuestion = ref(1);
+
+</script>
+
+<template>
+    <div>
+        <h1>QUIZ</h1>
+        <h2>?/?</h2>
+        <h2>Question ?</h2>
+        <div class="answer">
+            <input type="radio" id="choice1" value="answer1">     
+            <input type="radio" id="choice2" value="answer2">     
+            <input type="radio" id="choice3" value="answer3">     
+            <input type="radio" id="choice4" value="answer4">  
+        </div>
+        <button>Validate</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
