97839d6fcd1e622e3ab8b48e290f7b47474c3c09
Mathis <mathis.augereau@ynov.com>
2022-02-08 09:45:50 +0100

    fix

--- swapi48h/src/components/Theme.vue
+++ swapi48h/src/components/Theme.vue
@@ -1,31 +1,50 @@
 <script setup> 
-import store from '../store'
-import { ref, watch } from 'vue';
+ import store from '../store.js'
+    import { ref, watch } from 'vue';
     const elements = ref([]);
-    async function getDataFromTheme() {
-        let next;
-        elements.value = [];
+    let listEl = [];
+    async function getDataFromTheme(next) {
         const option = {
             method: "GET",
         };
-        next = 'https://swapi.dev/api/'+store.state.selectedTheme;
-        while(next){
-            const response = await fetch(next, option);
-            if(response.status == 200) {
-                response.json().then(data => {
-                    elements.value.concat(data.results); 
-                    next = data.next;
+        const response = await fetch(next, option);
+        if(response.status == 200) {
+            response.json().then(data => {
+                listEl = listEl.concat(data.results); 
+                if(data.next){
+                    getDataFromTheme(data.next);
+                }
+                elements.value = listEl;
             });
         }
+        //await console.log(element.value);
     }
-}
-watch(() => store.state.selectedTheme, () => {getDataFromTheme()})
+    watch(() => store.state.selectedTheme, () => {getDataFromTheme('https://swapi.dev/api/'+store.state.selectedTheme)})
+    getDataFromTheme('https://swapi.dev/api/'+store.state.selectedTheme);
 </script>
 
 
 <template>
+<div class="theme">
+    <h1> {{store.state.selectedTheme}}:</h1>
+</div>
+    <div class="element">
+    <ul>
+    <template v-for ="element in elements" :key="element">
+        <li v-if="store.state.selectedTheme == film">{{ element.title }}</li>
+        <li v-else>{{ element.name }}</li>
+    </template>
+    </ul>
+    </div>
 </template>
 
 
 <style scoped>
+.theme{
+    left: 10em;
+    position:absolute;
+}
+.element{
+margin-top: 10em;
+}
 </style>
